d2c8c8b26c0282216900708785b40e24da27160f
gabrielbagon1 <e-mail@exemplo.com>
2021-12-14 13:48:42 -0300

    Projeto Barbearia Rossini

---
+++ Login/scripts/main.js
@@ -0,0 +1,22 @@
+ var btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    return Enviar(e);
+})
+
+function Enviar() {
+    var nome = document.getElementById("nomeid")
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    if (email.value && password.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
